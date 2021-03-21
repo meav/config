159b0f656a39ede864854e2c8cd3954107f24c4e
meav <milanistavietnam@gmail.com>
2021-03-21 16:02:46 +0700

    Timer title = Current time

--- Timer/Timer.bat
+++ Timer/Timer.bat
@@ -1,7 +1,18 @@
 :while
-del /F "C:\Users\Administrator\Music\Timer\HourglassPortable.config"
-start "" /MIN "C:\Users\Administrator\Music\Timer\focus.bat"
-"C:\Users\Administrator\Music\Timer\HourglassPortable.exe" --always-on-top off --full-screen off --prompt-on-exit on --show-in-notification-area off --do-not-keep-awake off --show-time-elapsed off --loop-timer off --pop-up-when-expired on --close-when-expired on --shut-down-when-expired off --theme gray --sound none --loop-sound off --open-saved-timers off --window-title left --window-state normal --lock-interface off --window-bounds 300,150,350,150
-"C:\Users\Administrator\Music\Timer\HourglassPortable.exe" --always-on-top on --full-screen off --prompt-on-exit on --show-in-notification-area off --do-not-keep-awake off --show-time-elapsed off --loop-timer off --pop-up-when-expired on --close-when-expired on --shut-down-when-expired off --theme purple --sound none --loop-sound off --open-saved-timers off --window-title left --window-state normal --lock-interface off --window-bounds 20,20,3500,1500 150s
-"C:\Users\Administrator\Music\Timer\HourglassPortable.exe" --always-on-top on --full-screen off --prompt-on-exit on --show-in-notification-area off --do-not-keep-awake off --show-time-elapsed off --loop-timer off --pop-up-when-expired on --close-when-expired on --shut-down-when-expired off --theme green --sound none --loop-sound off --open-saved-timers off --window-title left --window-state normal --lock-interface off --window-bounds 20,20,3500,1500 30s
+
+setlocal EnableExtensions DisableDelayedExpansion
+for /F "tokens=2 delims==." %%I in ('%SystemRoot%\System32\wbem\wmic.exe OS GET LocalDateTime /VALUE') do set "LocalDateTime=%%I"
+set "LocalHour=%LocalDateTime:~8,2%"
+set "AM_PM=AM"
+if %LocalHour% LSS 12 goto CopyTime
+set "AM_PM=PM"
+if %LocalHour% GTR 12 set /A LocalHour-=12
+set "LocalHour=0%LocalHour%"
+set mytime=%LocalHour:~-2%:%LocalDateTime:~10,2% %AM_PM%
+
+del /F "C:\Users\VHDX\Music\Timer\HourglassPortable.config"
+start "" /MIN "C:\Users\VHDX\Music\Timer\focus.bat"
+"C:\Users\VHDX\Music\Timer\HourglassPortable.exe" --title "%mytime%" --always-on-top off --full-screen off --prompt-on-exit on --show-in-notification-area off --do-not-keep-awake off --show-time-elapsed off --loop-timer off --pop-up-when-expired on --close-when-expired on --shut-down-when-expired off --theme gray --sound none --loop-sound off --open-saved-timers off --window-title left --window-state normal --lock-interface off --window-bounds 20,20,3500,1500
+"C:\Users\VHDX\Music\Timer\HourglassPortable.exe" --always-on-top on --full-screen off --prompt-on-exit on --show-in-notification-area off --do-not-keep-awake off --show-time-elapsed off --loop-timer off --pop-up-when-expired on --close-when-expired on --shut-down-when-expired off --theme purple --sound none --loop-sound off --open-saved-timers off --window-title left --window-state normal --lock-interface off --window-bounds 20,20,3500,1500 150s
+"C:\Users\VHDX\Music\Timer\HourglassPortable.exe" --always-on-top on --full-screen off --prompt-on-exit on --show-in-notification-area off --do-not-keep-awake off --show-time-elapsed off --loop-timer off --pop-up-when-expired on --close-when-expired on --shut-down-when-expired off --theme green --sound none --loop-sound off --open-saved-timers off --window-title left --window-state normal --lock-interface off --window-bounds 20,20,3500,1500 30s
 goto :while
