3b8eaab2243d25e32425eb8a374c11cc5504ea86
meav <milanistavietnam@gmail.com>
2021-03-27 16:43:58 +0700

    Change start timer to ps1

--- Timer/StartTimer.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set /p minute="Minutes: " %=%
-if "%minute%"=="quit" ( goto :while ) else (
-       if "%minute%"=="" ( explorer "C:\Users\VHDX\Music\Timer\Timer.vbs" ) )
-:while
-pwsh-preview.cmd -new_console:t:"pwsh preview":d:C:\Users\VHDX\Desktop
---
+++ Timer/StartTimer.ps1
@@ -0,0 +1,13 @@
+do {
+  echo "`eD`eD`eD`eD`eD`eD`eD`eD`eD`eD`eD`eD`eD`eD`eD`eD`eD`eD`eD`eD`eD`eD`eD`eD`eD`e[25A"
+  $name = Read-Host 'What is your username?'
+  if ($name -ne "timer" -and $name -ne "quit")
+    {echo "`n"
+     echo "`e[31;40mWrong`e[0m"
+    }
+}
+until ($name -eq "timer" -or $name -eq "quit")
+if ($name -eq "timer") {explorer C:\Users\VHDX\Music\config\Timer\Timer.vbs
+                        cmd /c 'pwsh.exe -new_console:t:"pwsh":d:C:\Users\VHDX\Desktop'
+		       }
+  else {cmd /c 'pwsh.exe -new_console:t:"pwsh":d:C:\Users\VHDX\Desktop'}
